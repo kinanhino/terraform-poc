be328feb807b938a10923e4933e67888b70b3bf8
Kinan Hino <kinan@kaleidoo.ai>
2025-07-09 20:31:44 +0300

    initialize simple app

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Kubernetes World"
+
+@app.route('/temp')
+def another():
+    return "Hello, It's Temporary here"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
